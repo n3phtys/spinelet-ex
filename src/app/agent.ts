e47d6b5c37c866c987159dae27cb2e33e517db14
Nephtys <christopher.kaag@gmail.com>
2017-06-11 22:02:02 +0200

    added avatar urls to agents

--- src/app/agent.ts
+++ src/app/agent.ts
@@ -7,6 +7,7 @@ export class Agent {
 
     title: string;
     description: string;
+    avatarUrl: string;
     parryDV: number;
     evasionDV: number;
     soak: number;
@@ -32,13 +33,14 @@ export class Agent {
     conditions: CustomCondition[];
 
     // battlegroup
-    static Battlegroup(title: string, description: string,
+    static Battlegroup(title: string, description: string, avatarUrl: string,
         drill: Drill, size: number,
         might: number, basicHP: number, basicSoak: number, hardness: number,
         evasion: number, parry: number
         ): Agent {
             const entitity = new Agent();
         entitity.title = title;
+        entitity.avatarUrl = avatarUrl;
         entitity.description = description;
         entitity.parryDV = parry;
         entitity.evasionDV = evasion;
@@ -63,12 +65,13 @@ export class Agent {
 
 
     // weak individual
-    static WeakIndividual(soak: number, hardness: number, title: string, description: string,
+    static WeakIndividual(soak: number, hardness: number, title: string, description: string, avatarUrl: string,
         hpWPZero: number, hpWPOne: number, hpWPTwo: number, hpWPFour: number,
         evasion: number, parry: number
         ): Agent {
             const entitity = new Agent();
         entitity.title = title;
+        entitity.avatarUrl = avatarUrl;
         entitity.description = description;
         entitity.parryDV = parry;
         entitity.evasionDV = evasion;
@@ -93,12 +96,13 @@ export class Agent {
 
 
     // strong individual
-    static StrongIndividual(soak: number, hardness: number, title: string, description: string,
+    static StrongIndividual(soak: number, hardness: number, title: string, description: string, avatarUrl: string,
         hpWPZero: number, hpWPOne: number, hpWPTwo: number, hpWPFour: number,
         evasion: number, parry: number
         ): Agent {
             const entitity = new Agent();
         entitity.title = title;
+        entitity.avatarUrl = avatarUrl;
         entitity.description = description;
         entitity.parryDV = parry;
         entitity.evasionDV = evasion;
@@ -122,12 +126,13 @@ export class Agent {
     }
 
     // special individual
-    static SpecialIndividual(soak: number, hardness: number, title: string, description: string,
+    static SpecialIndividual(soak: number, hardness: number, title: string, description: string, avatarUrl: string,
         hpWPZero: number, hpWPOne: number, hpWPTwo: number, hpWPFour: number,
         evasion: number, parry: number
         ): Agent {
             const entitity = new Agent();
         entitity.title = title;
+        entitity.avatarUrl = avatarUrl;
         entitity.description = description;
         entitity.parryDV = parry;
         entitity.evasionDV = evasion;
@@ -216,11 +221,17 @@ export class Agent {
 export const PREMADE_AGENTS: Agent[] = [
     Agent.Battlegroup('Battlegroup (Battle Ready Troops)',
         'A standard unit of about 50 soldiers well trained and armed',
+        'https://qph.ec.quoracdn.net/main-qimg-f1d0a1149889f9bdd807f218fe533478-c',
         Drill.Average, 2, 0, 6, 8, 0, 2, 4),
-    Agent.WeakIndividual(6, 0, 'Militia soldier', 'The weakest kind of mortal soldier that is worth tracking.', 1, 2, 2, 1, 2, 2),
-    Agent.StrongIndividual(8, 4, 'Wyvern', 'A Wyvern. Some can be used as mounts', 3, 3, 3, 1, 4, 3),
-    Agent.SpecialIndividual(12, 4, 'Deathclaw', 'You should run now', 3, 5, 5, 4, 4, 6),
-    Agent.SpecialIndividual(14, 7, 'Arete-Series Artificial Hero', 'Sparring partner for Dawn Castes', 10, 0, 0, 0, 6, 7),
+    Agent.WeakIndividual(6, 0, 'Militia soldier', 'The weakest kind of mortal soldier that is worth tracking.',
+    'http://www.maps4heroes.com/heroes5/pictures/haven/HOMM5_Haven_Militia_guard.jpg', 1, 2, 2, 1, 2, 2),
+    Agent.StrongIndividual(8, 4, 'Wyvern', 'A Wyvern. Some can be used as mounts',
+    'http://vignette3.wikia.nocookie.net/criticalrole/images/e/e8/Wyvern.jpg/revision/latest?cb=20150810045153', 3, 3, 3, 1, 4, 3),
+    Agent.SpecialIndividual(12, 4, 'Deathclaw', 'You should run now',
+    'http://www.writeups.org/wp-content/uploads/Deathclaw-Fallout-h.jpg', 3, 5, 5, 4, 4, 6),
+    Agent.SpecialIndividual(14, 7, 'Arete-Series Artificial Hero', 'Sparring partner for Dawn Castes',
+    'http://webneel.com/daily/sites/default/files/images/daily/01-2013/14-robot-fighter.jpg', 10, 0, 0, 0, 6, 7),
     Agent.SpecialIndividual(16, 10, 'Experienced Dragon-Blood', 'A military directed Dragonblooded of Essence 3 carrying artifact weaponry',
+    'http://cdn.obsidianportal.com/assets/109213/DragonBloodedCover.jpg',
     1, 7, 7, 1, 0, 5)
 ] as Agent[];
